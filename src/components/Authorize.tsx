0d148602454dcf664a47b168bf90b416507f752d
Josh Daniel <joshpersonal8@gmail.com>
2021-09-11 20:19:51 +0800

    Update authorize page

--- src/components/Authorize.tsx
+++ src/components/Authorize.tsx
@@ -7,12 +7,15 @@ export const Authorize: React.FC = () => {
 
   return (
     <div className="w-[80%] flex flex-col items-center mt-32">
-      <h1 className="text-5xl font-bold mb-4 text-green-800">Trackify</h1>
+      <h1 className="text-5xl font-bold text-gray-900">Trackifire</h1>
+      <p>
+        <i>just another task-tracker 🔥</i>
+      </p>
       <div
-        className="flex items-center bg-green-700 text-gray-200 p-2 px-16 rounded-md cursor-pointer"
+        className="mt-8 flex items-center bg-gray-900 text-gray-200 p-2 px-16 rounded-md cursor-pointer hover:bg-gray-800 color-trans"
         onClick={signIn}
       >
-        <p className="mr-2">sign in</p>
+        <p className="mr-2">sign in with google</p>
         <AiOutlineGoogle />
       </div>
     </div>
